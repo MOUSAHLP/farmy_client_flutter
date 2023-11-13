912f529bab97593476b6abcd019bdcf666b5cf65
anwar <samisari367@gmail.com>
2023-11-13 07:18:42 +0000

    add basic color

--- lib/presentation/resources/color_manager.dart
+++ lib/presentation/resources/color_manager.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ColorManager {
-  static const Color primary=Color(0xff4F8640);
-  static  Color lightPrimaryColor=const Color(0xff4F8640).withOpacity(0.65);
-
+  static const primaryGreen = Color(0xff4F8640);
+  static const lightGreen = Color(0xff172F20);
+  static const darkGreen = Color(0xff224A32);
+  static const lightGray = Color(0xff224A32);
 }
