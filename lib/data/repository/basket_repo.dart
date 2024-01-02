3c9023572b6325ceee1a40b8717e3d285d325d66
anwar <samisari367@gmail.com>
2024-01-02 13:51:59 +0000

    create basket repo

---
+++ lib/data/repository/basket_repo.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:pharma/core/utils/api_const.dart';
+import 'package:pharma/data/data_resource/remote_resource/api_handler/base_api_client.dart';
+import 'package:pharma/models/params/payment_process_parms.dart';
+import 'package:pharma/models/payment_process_response.dart';
+
+class BasketRepo {
+  Future<Either<String, PaymentProcessResponse>> getPaymentDetails(
+      PaymentProcessParms paymentProcessParms) {
+    return BaseApiClient.post<PaymentProcessResponse>(
+        formData: paymentProcessParms.toJson(),
+        url: ApiConst.getPaymentDetails,
+        converter: (e) {
+          return PaymentProcessResponse.fromJson(e["data"]);
+        });
+  }
+}
