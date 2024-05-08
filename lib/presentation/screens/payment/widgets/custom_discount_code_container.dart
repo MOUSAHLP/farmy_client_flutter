6bffb5cb8ca554f86dfb00d228812fedca448e08
Ahmad Mdadeh <eng.ahmadmdadeh@gmail.com>
2024-05-08 14:26:49 +0300

    __

--- lib/presentation/screens/payment/widgets/custom_discount_code_container.dart
+++ lib/presentation/screens/payment/widgets/custom_discount_code_container.dart
@@ -5,7 +5,6 @@ import 'package:pharma/bloc/basket_bloc/basket_bloc.dart';
 import 'package:pharma/bloc/location_bloc/location_bloc.dart';
 import 'package:pharma/bloc/my_order_bloc/my_order_bloc.dart';
 import 'package:pharma/bloc/payment_bloc/payment_bloc.dart';
-import 'package:pharma/models/delivery_response.dart';
 import 'package:pharma/models/params/Invoices_params.dart';
 import 'package:pharma/models/reward/reward_coupons_fixed_value.dart';
 import 'package:pharma/presentation/resources/color_manager.dart';
